b57da606a4526f9bceab189532297ab114f5d1f0
R-Aravind <aravind.ar786@gmail.com>
2019-07-22 20:44:50 +0530

    home updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  clickCounter: number = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  countClick() {
+    this.clickCounter += 1;
+  }
 }
